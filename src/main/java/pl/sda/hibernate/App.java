5ef8a2507ee60d85a352ca01b291ab621f2ae9f7
lukaszog <lukasz.ogan@gmail.com>
2019-08-11 11:58:21 +0200

    add hibernateutils

--- src/main/java/pl/sda/hibernate/App.java
+++ src/main/java/pl/sda/hibernate/App.java
@@ -14,19 +14,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        SessionFactory sf = new Configuration()
-                .configure()
-                .buildSessionFactory();
+//     \   SessionFactory sf = new Configuration()
+//                .configure()
+//                .buildSessionFactory();
 
-        Session session = sf.openSession();
+        Session session = HibernateUtils.getSession();
 
         User user = new User("Jan", "Kowalski");
 
         Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }finally {
+            if(session != null){
+               HibernateUtils.closeSession();
+            }
+        }
+        session = HibernateUtils.getSession();
         transaction = session.beginTransaction();
-        session.save(user);
-        transaction.commit();
-
-        session.close();
+        session.save(new User("jan", "kowalski"));
+        HibernateUtils.closeSession();
     }
 }
